2c178d0ac21753e8ff7cadd207800b675eb1cf53
Александар Стефановић <stefanovicsalex@gmail.com>
2022-05-22 13:50:01 +0200

    Форма логин

---
+++ Aplikacija_balon/Login.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Aplikacija_balon
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void dugme_login_Click(object sender, EventArgs e)
+        {
+            if (txt_email.Text == "" || txt_lozinka.Text == "")
+            {
+                MessageBox.Show("Niste uneli podatke.");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection veza = Konekcija.Povezi();
+                    SqlCommand komanda = new SqlCommand("SELECT * FROM Osoba WHERE email = @username", veza);
+                    komanda.Parameters.AddWithValue("@username", txt_email.Text);
+                    SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+
+                    int brojac = tabela.Rows.Count;
+                    if (brojac == 1)
+                    {
+                        if (String.Compare(tabela.Rows[0]["pass"].ToString(), txt_pass.Text) == 0)
+                        {
+                            MessageBox.Show("Успешно сте се улоговали!");
+                            Program.ime_korisnika = tabela.Rows[0]["ime"].ToString();
+                            Program.prezime_korisnika = tabela.Rows[0]["prezime"].ToString();
+                            Program.uloga_korisnika = tabela.Rows[0]["uloga"].ToString();
+                            this.Hide();
+                            Glavna2 frm_Glavna2 = new Glavna2();
+                            frm_Glavna2.Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Унели сте погрешну лозинку!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Унели сте непостојећу имејл адресу!");
+                    }
+                }
+                catch (Exception greska)
+                {
+                    MessageBox.Show(greska.Message);
+                }
+            }
+        }
+    }
+}
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -37,3 +37,17 @@ datum date not null,
 pocetak int not null,
 kraj int not null
 );
+
+insert into Korisnik values('milanmaric@gmail.com', 'milan123', 'Milan', 'Maric')
+insert into Korisnik values('dusanstankovic@gmail.com', 'duleCR7', 'Dusan', 'Stankovic')
+insert into Korisnik values('bogdankolovic@gmail.com', 'autizmo', 'Bogdan', 'Kolovic')
+insert into Korisnik values('andrejbratic@gmail.com', 'bratke123', 'Andrej', 'Bratic')
+select * from korisnik
+
+insert into Objekat values('Balon Juzni Bulevar', 'Juzni bulevar 69', 'Parking, tusevi, teren za 5 na 5 ili 6 na 6')
+insert into Objekat values('Zlatna lopta Vracar', 'Djerdapska 19', 'Parking, tusevi, teren za 5 na 5 ili 6 na 6')
+insert into Objekat values('Imperija sport', 'Vukosaviceva 21a', 'Parking, tusevi, teren za 5 na 5')
+select * from Objekat
+
+insert into zaposleni values('milanmariczp@gmail.com', 'milan123', 'Milan', 'Maric', 1)
+select * from zaposleni
